cb0fde5eaa5b183e5d1ffab5caf3e17c328ba0fa
asgardus <gandhiansh95@gmail.com>
2018-04-06 23:12:19 +0200

    Task 1 Initial Commit

--- PID_intro.m
+++ PID_intro.m
@@ -11,15 +11,15 @@ clc
 clear all
 
 %% mass, spring and damper values and transfer function
-M = 20;                     % mass in kg
-K = 400;                    % spring stiffness coeficient in N/m
-C = 200;                    % damping coefficient in Ns/m    
+M = 423.23;                     % mass in kg
+K = 20000;                    % spring stiffness coeficient in N/m
+C = 3800;                    % damping coefficient in Ns/m    
 s = tf('s');                % LaPlace parameter
 t = 0:0.01:2;
 %% plot the step response of the different controllers (P,PD,PI and PID)
-KP = 0;
-KI = 0;
-KD = 0;
+KP = 37500;
+KI = 175000;
+KD = 5000;
 figure(3)
 set(gcf,'Position',[100 100 600 600])
 grid on
@@ -31,25 +31,25 @@ ylabel('Amplitude')
 ylim([-0.5 1.5])
 % Slider erzeugen
 KP_Slider = uicontrol('Style', 'slider',...
-'Position', [100 75 120 20],'min',0,'max',10000,...
+'Position', [100 75 120 20],'min',0,'max',1e6,'SliderStep',[0.05 0.1],...
 'value',KP,'String','Kp');
 text(0.2,-0.33,'K_{p}=','FontSize',14)
 KP_wert=text(0.4,-0.33,'test','FontSize',14);
 
 KI_Slider = uicontrol('Style', 'slider',...
-'Position', [250 75 120 20],'min',0,'max',10000,...
+'Position', [250 75 120 20],'min',0,'max',1e6,'SliderStep',[0.05 0.1],...
 'value',KI,'String','Ki');
 text(0.8,-0.33,'K_{i}=','FontSize',14)
 KI_wert=text(1.0,-0.33,num2str(KI),'FontSize',14);
 
 KD_Slider = uicontrol('Style', 'slider',...
-'Position', [400 75 120 20],'min',0,'max',2000,...
+'Position', [400 75 120 20],'min',0,'max',2e5,'SliderStep',[0.05 0.1],...
 'value',KD,'String','Kd');
 text(1.5,-0.33,'K_{d}=','FontSize',14)
 KD_wert=text(1.7,-0.33,num2str(KD),'FontSize',14);
 
 M_Slider = uicontrol('Style', 'slider',...
-'Position', [100 125 120 20],'min',1,'max',400,...
+'Position', [100 125 120 20],'min',1,'max',450,...
 'value',M,'String','M');
 text(0.2,-0.1,'M=','FontSize',14)
 M_wert=text(0.4,-.1,num2str(M),'FontSize',14);
@@ -61,7 +61,7 @@ text(0.8,-0.1,'K=','FontSize',14)
 K_wert=text(1.0,-0.1,num2str(K),'FontSize',14);
 
 C_Slider = uicontrol('Style', 'slider',...
-'Position', [400 125 120 20],'min',0,'max',2000,...
+'Position', [400 125 120 20],'min',0,'max',5000,...
 'value',C,'String','C');
 text(1.5,-0.1,'C=','FontSize',14)
 C_wert=text(1.7,-0.1,num2str(C),'FontSize',14);
@@ -73,11 +73,12 @@ KD = get(KD_Slider,'value');
 M = get(M_Slider,'value');
 K = get(K_Slider,'value');
 C = get(C_Slider,'value');
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Implement the transfer function of the equation of motion here:
 % Use variable s for the first derivative of x and s^2 for second
 % derivative of x
-T =   
+T = 1/(M*s^2+C*s+K);  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Ctrl = pid(KP,KI,KD);
 S = feedback(Ctrl*T,1);
