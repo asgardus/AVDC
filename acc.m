25727f62cf9b5a92ec47244a33bf3c48f749a189
Ankur Fartyal <37195694+ankurf78@users.noreply.github.com>
2018-04-11 23:14:13 +0200

    11pm

--- acc.m
+++ acc.m
@@ -3,18 +3,21 @@ clear all
 k_t=0.7356; 
 k_b=0.7268;
 d=1;
-pc=[0];
-ic=[100 300 750 1200 2000];
-dc=[100 300 750 1200 2000];
-for mu_select = 1:3;              % set friction to mu_select = 1 (dry road), 2 (wet road) or 3 (snow) for road and 1 for rail
+pc=[-10000 0 0];
+% ic=[100 300 750 1200 2000];
+dc=[0 0 0];
+for k=1:3           % set friction to mu_select = 1 (dry road), 2 (wet road) or 3 (snow) for road and 1 for rail
 % for j=1:length(pc);    
-    P_t=20000;
-    I_t=200;
-    D_t=1500;
-   
-    P_b=-10000;
-    D_b=0;
-    I_b=0;
+%     P_t=20000;
+%     I_t=200;
+%     D_t=1500;
+%    
+%     P_b=-10000;
+%     D_b=0;
+%     I_b=0;
+p=pc(k)
+% i=ic(k)
+di=dc(k)
     initial;
     simOut = sim('slip_model_Student.mdl');
 
@@ -26,7 +29,7 @@ for mu_select = 1:3;              % set friction to mu_select = 1 (dry road), 2 
     plot(veh_x);
     hold on
 
-    i=1;
+
     for i=1:length(veh_x.time)      %loop for finding shortest acceleration time #gives k_t
         if veh_x.Data(i) >= 25
             acc_t(d)=veh_x.time(i);
@@ -39,10 +42,10 @@ for mu_select = 1:3;              % set friction to mu_select = 1 (dry road), 2 
         %j=i is used so that the time is obtained after reaching 25m/s,
         %subtract 3seconds to obtain braking time, and 3*25 m from the braking distance. 
         if veh_x.Data(j) <= 0.000
-            b_t(d)=veh_x.time(j)-(3); %add acc_t when removing for loop %the time taken from application of brake to vehicle halt
-            b_d(d)=veh_dist.data(j)-(75); %add acc_d when removing for loop%the dist taken from application of brake to vehicle halt
+            b_t(d)=veh_x.time(j)-(3+3.7696); %add acc_t when removing for loop %the time taken from application of brake to vehicle halt
+            b_d(d)=veh_dist.data(j)-(75+45.7931); %add acc_d when removing for loop%the dist taken from application of brake to vehicle halt
             wheel_b(d)=wheel_x.Data(j);
-           
+           m=1
             
             break
         end
@@ -50,9 +53,9 @@ for mu_select = 1:3;              % set friction to mu_select = 1 (dry road), 2 
     d=d+1;
 end
 % end
-close(h)
-figure(3);
-plot(acc_t)
+% close(h)
+% figure(3);
+% plot(acc_t)
 
-% figure(4);
-% plot(b_d)
+figure(4);
+plot(b_d)
