2a8a30aaf467465cb4982d41cf1e0e2b14316e7d
asgardus <gandhiansh95@gmail.com>
2018-04-19 14:27:40 +0200

    Final Update

--- ExtraTask_1d.m
+++ ExtraTask_1d.m
@@ -3,14 +3,17 @@ M = 395;                     % mass in kg
 K = 20000;                    % spring stiffness coeficient in N/m
 C = 3800;                    % damping coefficient in Ns/m    
 s = tf('s');                % LaPlace parameter
-%% plot the step response of the different controllers (P,PD,PI and PID)
+%% enter control parameters
+
 KP = 6250;
 KI = 66250;
 KD = 0;
 T = 1/(M*s^2+C*s+K);  
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% plot nyquist and pole zero maps for the system defined above
+
 Ctrl = pid(KP,KI,KD);
 S = feedback(Ctrl*T,1);
-
-nyquist(T);
+figure(1);
+nyquist(S);
+figure(2);
 pzmap(T);
